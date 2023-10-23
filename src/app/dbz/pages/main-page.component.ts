f5fd4f409f1d491a8746a9a75f92d0ed74aff13f
Alejandro Toledo <alejandrotoledo@pop-os.localdomain>
2023-10-23 15:11:00 -0500

    make a two ways suscription

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -11,4 +11,9 @@ export class MainPageComponent {
     { name: 'Goku', power: 9500 },
     { name: 'Vegeta', power: 7500 },
   ];
+
+  onNewCharacter(character: Character): void {
+    console.log('main page');
+    console.log(character);
+  }
 }
