93312c27c475b3f07804a2fcae71774c87e031ac
Alejandro Toledo <alejandrotoledo@pop-os.localdomain>
2023-10-23 12:19:19 -0500

    add ngFor and add two lists

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
-    selector: 'app-dbz-main-page',
-    templateUrl: './main-page.component.html'
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
 })
-
 export class MainPageComponent {
-
+  public characters: Character[] = [
+    { name: 'Krillin', power: 500 },
+    { name: 'Goku', power: 9500 },
+    { name: 'Vegeta', power: 7500 },
+  ];
 }
